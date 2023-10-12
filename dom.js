6048b3b9488de00bea345d7d43046d07f7a9689b
dieuhan <dieuhan.mut@gmail.com>
2023-10-12 13:11:23 -0400

    yay

--- dom.js
+++ dom.js
@@ -1,5 +1,4 @@
-
-DOMSelctors = {
+DOMSelectors = {
     // store all related code for selecting elements here
         form: document.querySelector("#form"),
         firstname: document.querySelector(".first-name"),
@@ -8,9 +7,8 @@ DOMSelctors = {
     console.log(DOMSelectors.h2s)
     DOMSelectors.form.addEventListener("submit", function (event) {
       event.preventDefault();
-        DOMSelectors.h2s.forEach((header)=> header.textContext = DOMSelectors.firstname.value
+        DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.firstname.value
         );
-  
     });
 
 
