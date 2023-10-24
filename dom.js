d8e1f31a10bc747b3420f98026dcd93fd560965d
dieuhan <dieuhan.mut@gmail.com>
2023-10-24 13:15:17 -0400

    SGDJBFUOWEBVGIVGFWOVGFOUI

--- dom.js
+++ dom.js
@@ -3,13 +3,20 @@ DOMSelectors = {
         form: document.querySelector("#form"),
         firstname: document.querySelector(".first-name"),
         h2s: document.querySelectorAll("h2"),
+        form2: document.querySelector("#form"),
+        lastname: document.querySelector(".last-name"),
+
     };
+
     console.log(DOMSelectors.h2s)
     DOMSelectors.form.addEventListener("submit", function (event) {
       event.preventDefault();
-        DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.firstname.value
+        DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.firstname.value, DOMSelectors.lastname.value,
+      
         );
     });
+
+      
 
 
 
