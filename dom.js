8eb3068ff4235437d854a296526be51e04798f5c
dieuhan <dieuhan.mut@gmail.com>
2023-11-07 15:25:12 -0500

    vjgvgc

--- dom.js
+++ dom.js
@@ -1,25 +1,3 @@
-/* const DOMSelectors = {
-    // store all related code for selecting elements here
-        form: document.querySelector("#form"),
-        pokemon: document.querySelector(".pokemon-name"),
-        h2s: document.querySelectorAll("h2"),
-        type: document.querySelector(".ele-type"),
-        img: document.querySelector("image"),
-        card: document.querySelector("card")
-
-    };
-
-   
-
-    console.log(DOMSelectors.card)
-    DOMSelectors.form.addEventListener("submit", function (event) {
-      event.preventDefault();
-        DOMSelectors.card.forEach((header)=> header.textContent = DOMSelectors.card, 
-      
-        );
-    });
- */
-
 const DOMSelectors = {
 pokemon: document.querySelector(".pokemon-name"),
 img: document.querySelector(".image"),
