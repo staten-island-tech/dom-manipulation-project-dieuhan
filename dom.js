032f54e7754b83163f75c94335144fba70bee8f0
dieuhan <dieuhan.mut@gmail.com>
2023-11-08 11:32:43 -0500

    jh

--- dom.js
+++ dom.js
@@ -20,42 +20,44 @@
     });
  */
 
-const DOMSelectors = {
-pokemon: document.querySelector(".pokemon-name"),
-type: document.querySelector(".ele-type"),
-img: document.querySelector(".image"),
-card: document.querySelector(".card"),
-input: document.querySelector(".input"),
-gallery: document.querySelector(".gallery"),
-form: document.querySelector("#form"),
-};
 
-
-console.log(DOMSelectors);
-DOMSelectors.form.addEventListener("submit", function (event){
-  event.preventDefault();
-  console.log(DOMSelectors.pokemon.value)
-  console.log(DOMSelectors.type.value)
-  console.log(DOMSelectors.img.value)
-  
-
-  function card (){
-    DOMSelectors.gallery.insertAdjacentHTML("afterbegin", `<div class="card"> <h2 class="names"> ${DOMSelectors.pokemon.value}  
-    ${DOMSelectors.type.value} <img class ="img-edit" src=${DOMSelectors.img.value} alt=""> </h2>  </div> `)
-    
-  };
-  card()});
-
- function remove(){
-    let buttons = document.querySelectorAll("button")
-    buttons.forEach((btn)=> btn.addEventListener('click', function(event){
-      console.log(event.target.parentElement);
-    })
-    );
-  }
-  
-  remove(); 
-  
+    const DOMSelectors = {
+      pokemon: document.querySelector(".pokemon-name"),
+      type: document.querySelector(".ele-type"),
+      img: document.querySelector(".image"),
+      card: document.querySelector(".card"),
+      input: document.querySelector(".input"),
+      gallery: document.querySelector(".gallery"),
+      form: document.querySelector("#form"),
+      };
+      
+      
+      console.log(DOMSelectors);
+      DOMSelectors.form.addEventListener("submit", function (event){
+        event.preventDefault();
+        console.log(DOMSelectors.pokemon.value)
+        console.log(DOMSelectors.type.value)
+        console.log(DOMSelectors.img.value)
+        
+      
+        function card (){
+          DOMSelectors.gallery.insertAdjacentHTML("afterbegin", `<div class="card"> <h2 class="names"> ${DOMSelectors.pokemon.value}  
+          ${DOMSelectors.type.value} <img class ="img-edit" src=${DOMSelectors.img.value} alt=""> </h2>  </div> `)
+          
+        };
+        card()});
+      
+       /* function remove(){
+          let buttons = document.querySelectorAll("button")
+          buttons.forEach((btn)=> btn.addEventListener('click', function(event){
+            console.log(event.target.parentElement);
+          })
+          );
+        }
+        
+        remove(); 
+        
+   */
     
 
 
