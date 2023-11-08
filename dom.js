d6d3a8c771ed85572e1825e73826a9cba97b5271
dieuhan <dieuhan.mut@gmail.com>
2023-11-08 11:36:00 -0500

    ihfoweif

--- dom.js
+++ dom.js
@@ -1,65 +1,3 @@
-<<<<<<< HEAD
-/* const DOMSelectors = {
-    // store all related code for selecting elements here
-        form: document.querySelector("#form"),
-        pokemon: document.querySelector(".pokemon-name"),
-        h2s: document.querySelectorAll("h2"),
-        type: document.querySelector(".ele-type"),
-        img: document.querySelector("image"),
-        card: document.querySelector("card")
-
-    };
-
-   
-
-    console.log(DOMSelectors.card)
-    DOMSelectors.form.addEventListener("submit", function (event) {
-      event.preventDefault();
-        DOMSelectors.card.forEach((header)=> header.textContent = DOMSelectors.card, 
-      
-        );
-    });
- */
-
-
-    const DOMSelectors = {
-      pokemon: document.querySelector(".pokemon-name"),
-      type: document.querySelector(".ele-type"),
-      img: document.querySelector(".image"),
-      card: document.querySelector(".card"),
-      input: document.querySelector(".input"),
-      gallery: document.querySelector(".gallery"),
-      form: document.querySelector("#form"),
-      };
-      
-      
-      console.log(DOMSelectors);
-      DOMSelectors.form.addEventListener("submit", function (event){
-        event.preventDefault();
-        console.log(DOMSelectors.pokemon.value)
-        console.log(DOMSelectors.type.value)
-        console.log(DOMSelectors.img.value)
-        
-      
-        function card (){
-          DOMSelectors.gallery.insertAdjacentHTML("afterbegin", `<div class="card"> <h2 class="names"> ${DOMSelectors.pokemon.value}  
-          ${DOMSelectors.type.value} <img class ="img-edit" src=${DOMSelectors.img.value} alt=""> </h2>  </div> `)
-          
-        };
-        card()});
-      
-       /* function remove(){
-          let buttons = document.querySelectorAll("button")
-          buttons.forEach((btn)=> btn.addEventListener('click', function(event){
-            console.log(event.target.parentElement);
-          })
-          );
-        }
-        
-        remove(); 
-        
-   */
-=======
 const DOMSelectors = {
 pokemon: document.querySelector(".pokemon-name"),
 img: document.querySelector(".image"),
@@ -112,7 +50,7 @@ DOMSelectors.form.addEventListener("submit", function (event){
   
  
   
->>>>>>> 1d1d1ba6e631f599503af42c89b102ae203ccbd9
+
     
 
 
